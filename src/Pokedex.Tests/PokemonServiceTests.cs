0a343bbd30c0e1aa3ec9a7de8f63efc020db8b4d
Roberto Denisi <rdenisi@users.noreply.github.com>
2024-04-04 16:54:48 +0000

    Hotfix behavior and test for PokemonService

--- src/Pokedex.Tests/PokemonServiceTests.cs
+++ src/Pokedex.Tests/PokemonServiceTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Logging;
 using Moq;
 using Pokedex.Dtos;
 using Pokedex.Services;
@@ -7,174 +8,182 @@ using System.Net.Http.Json;
 
 namespace Pokedex.Tests
 {
-	public class PokemonServiceTests
-	{
-		private readonly PokemonService pokemonService;
-		private readonly MockHttpMessageHandler handlerMock = new();
-		private readonly Mock<IHttpClientFactory> httpClientFactoryMock = new();
-		private readonly string pokeapiBaseUrl = "https://pokeapi.co/api/v2/pokemon-species";
-		private readonly string pokeApiClientName = "PokeApi";
-		private readonly string funTranslationsClientName = "FunTranslations";
+    public class PokemonServiceTests
+    {
+        private readonly PokemonService pokemonService;
+        private readonly MockHttpMessageHandler handlerMock = new();
+        private readonly Mock<IHttpClientFactory> httpClientFactoryMock = new();
+        private readonly Mock<ILogger<PokemonService>> loggerMock = new();
+        private readonly string pokeapiBaseUrl = "https://pokeapi.co/api/v2/pokemon-species";
+        private readonly string pokeApiClientName = "PokeApi";
+        private readonly string funTranslationsClientName = "FunTranslations";
 
 
-		public PokemonServiceTests()
-		{
-			pokemonService = new PokemonService(httpClientFactoryMock.Object);
-		}
+        public PokemonServiceTests()
+        {
+            pokemonService = new PokemonService(loggerMock.Object, httpClientFactoryMock.Object);
+        }
 
-		[Fact]
-		public async Task GetPokemonAsync_WhenArgumentIsNullOrWhiteSpace_ShouldThrowArgumentNullException()
-		{
-			//Arrange
-			string? nullName = null;
+        [Fact]
+        public async Task GetPokemonAsync_WhenArgumentIsNullOrWhiteSpace_ShouldThrowArgumentNullException()
+        {
+            //Arrange
+            string? nullName = null;
 
-			//Act
-			//Assert
-			await Assert.ThrowsAsync<ArgumentNullException>(() => pokemonService.GetPokemonAsync(nullName));
-		}
+            //Act
+            //Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(() => pokemonService.GetPokemonAsync(nullName));
+        }
 
-		[Fact]
-		public async Task GetPokemonAsync_WhenNameIsNotFound_ShouldThrowKeyNotFoundException()
-		{
-			//Arrange
-			var notExistentName = Guid.NewGuid().ToString();
-			var url = $"{pokeapiBaseUrl}/{notExistentName}";
+        [Fact]
+        public async Task GetPokemonAsync_WhenNameIsNotFound_ShouldThrowKeyNotFoundException()
+        {
+            //Arrange
+            var notExistentName = Guid.NewGuid().ToString();
+            var url = $"{pokeapiBaseUrl}/{notExistentName}";
 
-			handlerMock
-				.When(url)
-				.Respond(HttpStatusCode.NotFound, JsonContent.Create(new { }));
+            handlerMock
+                .When(url)
+                .Respond(HttpStatusCode.NotFound, JsonContent.Create(new { }));
 
-			httpClientFactoryMock.Setup(x => x.CreateClient(pokeApiClientName))
-				.Returns(new HttpClient(handlerMock));
+            httpClientFactoryMock.Setup(x => x.CreateClient(pokeApiClientName))
+                .Returns(new HttpClient(handlerMock));
 
-			//Act
-			//Assert
-			await Assert.ThrowsAsync<KeyNotFoundException>(() => pokemonService.GetPokemonAsync(notExistentName));
-		}
+            //Act
+            //Assert
+            await Assert.ThrowsAsync<KeyNotFoundException>(() => pokemonService.GetPokemonAsync(notExistentName));
+        }
 
-		[Fact]
-		public async Task GetPokemonAsync_WhenNameExists_ShouldReturnAValidPokemon()
-		{
-			//Arrange
-			var existentName = "existentPokemonName";
-			var url = $"{pokeapiBaseUrl}/{existentName}";
+        [Fact]
+        public async Task GetPokemonAsync_WhenNameExists_ShouldReturnAValidPokemon()
+        {
+            //Arrange
+            var existentName = "existentPokemonName";
+            var url = $"{pokeapiBaseUrl}/{existentName}";
 
-			handlerMock
-				.When(url)
-				.Respond(HttpStatusCode.OK, JsonContent.Create(GetFakePokemonApiResponse(existentName, string.Empty)));
+            handlerMock
+                .When(url)
+                .Respond(HttpStatusCode.OK, JsonContent.Create(GetFakePokemonApiResponse(existentName, string.Empty)));
 
-			httpClientFactoryMock.Setup(x => x.CreateClient(pokeApiClientName))
-				.Returns(new HttpClient(handlerMock));
+            httpClientFactoryMock.Setup(x => x.CreateClient(pokeApiClientName))
+                .Returns(new HttpClient(handlerMock));
 
-			//Act
-			var result = await pokemonService.GetPokemonAsync(existentName);
+            //Act
+            var result = await pokemonService.GetPokemonAsync(existentName);
 
-			//Assert
-			Assert.True(result is not null);
-			Assert.False(string.IsNullOrEmpty(result.Name));
-		}
+            //Assert
+            Assert.True(result is not null);
+            Assert.False(string.IsNullOrEmpty(result.Name));
+        }
 
-		[Fact]
-		public async Task TranslateAsync_WhenArgumentIsNullOrWhiteSpace_ShouldThrowArgumentNullException()
-		{
-			//Arrange
-			string? nullName = null;
+        [Fact]
+        public async Task TranslateAsync_WhenArgumentIsNullOrWhiteSpace_ShouldThrowArgumentNullException()
+        {
+            //Arrange
+            string? nullName = null;
 
-			//Act
-			//Assert
-			await Assert.ThrowsAsync<ArgumentNullException>(() => pokemonService.TranslateAsync(nullName));
-		}
+            //Act
+            //Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(() => pokemonService.TranslateAsync(nullName));
+        }
 
-		[Fact]
-		public async Task TranslateAsync_WhenApiResponseIsNoSuccessCode_ShouldThrowAnExceptionWithTheErrorMessage()
-		{
-			//Arrange
-			var existentName = "existentPokemonName";
-			var description = "Pokemon description";
-			var pokeApiUrl = $"{pokeapiBaseUrl}/{existentName}";
+        [Fact]
+        public async Task TranslateAsync_WhenApiResponseIsNoSuccessCode_ShouldLogTheErrorAndReturnTheDefaultDescription()
+        {
+            //Arrange
+            var existentName = "existentPokemonName";
+            var description = "Pokemon description";
+            var pokeApiUrl = $"{pokeapiBaseUrl}/{existentName}";
 
-			var translationType = "yoda";
-			var text = description;
-			var funTranslationsUrl = $"https://api.funtranslations.com/translate/{translationType}.json?text={text}";
-			var jsonContent = JsonContent.Create(new { error = "An error occured!" });
-			var error = await jsonContent.ReadAsStringAsync();
-			var expectedExceptionMessage = $"An error occured calling the pokeapi service: [{error}]";
+            var translationType = "yoda";
+            var text = description;
+            var funTranslationsUrl = $"https://api.funtranslations.com/translate/{translationType}.json?text={text}";
+            var jsonContent = JsonContent.Create(new { error = "An error occured!" });
 
-			handlerMock
-				.When(pokeApiUrl)
-				.Respond(HttpStatusCode.OK, JsonContent.Create(GetFakePokemonApiResponse(existentName, description))); ;
+            var fakePokemonApiResponse = GetFakePokemonApiResponse(existentName, description);
 
-			handlerMock
-				.When(funTranslationsUrl)
-				.Respond(HttpStatusCode.InternalServerError, jsonContent);
+            handlerMock
+                .When(pokeApiUrl)
+                .Respond(HttpStatusCode.OK, JsonContent.Create(fakePokemonApiResponse)); ;
 
-			httpClientFactoryMock.Setup(x => x.CreateClient(pokeApiClientName))
-				.Returns(new HttpClient(handlerMock));
+            handlerMock
+                .When(funTranslationsUrl)
+                .Respond(HttpStatusCode.InternalServerError, jsonContent);
 
-			httpClientFactoryMock.Setup(x => x.CreateClient(funTranslationsClientName))
-				.Returns(new HttpClient(handlerMock));
+            httpClientFactoryMock.Setup(x => x.CreateClient(pokeApiClientName))
+                .Returns(new HttpClient(handlerMock));
 
-			//Act
-			//Assert
-			var exception = await Assert.ThrowsAsync<Exception>(() => pokemonService.TranslateAsync(existentName));
-			Assert.Equal(expectedExceptionMessage, exception.Message);
+            httpClientFactoryMock.Setup(x => x.CreateClient(funTranslationsClientName))
+                .Returns(new HttpClient(handlerMock));
 
-		}
+            //Act
+            var result = await pokemonService.TranslateAsync(existentName);
 
-		[Fact]
-		public async Task TranslateAsync_WhenApiResponseIsSuccessCode_ShouldReturnTranslatedPokemonDescription()
-		{
-			//Arrange
-			var existentName = "existentPokemonName";
-			var description = "Pokemon description";
-			var descriptionTranlsated = "Pokemon description translated";
-			var pokeApiUrl = $"{pokeapiBaseUrl}/{existentName}";
+            //Assert
+            Assert.Equal(result.Description, PokemonService.ApiResponseToDto(fakePokemonApiResponse)?.Description);
 
-			var translationType = "yoda";
-			var funTranslationsUrl = $"https://api.funtranslations.com/translate/{translationType}.json?text={description}";
-			var jsonContent = JsonContent.Create(GetFakeFunTranslationsApiResponse(description, descriptionTranlsated));
+            loggerMock.Verify(x => x.Log(
+                It.Is<LogLevel>(l => l == LogLevel.Error),
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((o, t) => true),
+                It.IsAny<Exception>(),
+                It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)), Times.Once);
+        }
 
-			handlerMock
-				.When(pokeApiUrl)
-				.Respond(HttpStatusCode.OK, JsonContent.Create(GetFakePokemonApiResponse(existentName, description))); ;
+        [Fact]
+        public async Task TranslateAsync_WhenApiResponseIsSuccessCode_ShouldReturnTranslatedPokemonDescription()
+        {
+            //Arrange
+            var existentName = "existentPokemonName";
+            var description = "Pokemon description";
+            var descriptionTranlsated = "Pokemon description translated";
+            var pokeApiUrl = $"{pokeapiBaseUrl}/{existentName}";
 
-			handlerMock
-				.When(funTranslationsUrl)
-				.Respond(HttpStatusCode.OK, jsonContent);
+            var translationType = "yoda";
+            var funTranslationsUrl = $"https://api.funtranslations.com/translate/{translationType}.json?text={description}";
+            var jsonContent = JsonContent.Create(GetFakeFunTranslationsApiResponse(description, descriptionTranlsated));
 
-			httpClientFactoryMock.Setup(x => x.CreateClient(pokeApiClientName))
-				.Returns(new HttpClient(handlerMock));
+            handlerMock
+                .When(pokeApiUrl)
+                .Respond(HttpStatusCode.OK, JsonContent.Create(GetFakePokemonApiResponse(existentName, description))); ;
 
-			httpClientFactoryMock.Setup(x => x.CreateClient(funTranslationsClientName))
-				.Returns(new HttpClient(handlerMock));
+            handlerMock
+                .When(funTranslationsUrl)
+                .Respond(HttpStatusCode.OK, jsonContent);
 
-			//Act
-			var result = await pokemonService.TranslateAsync(existentName);
+            httpClientFactoryMock.Setup(x => x.CreateClient(pokeApiClientName))
+                .Returns(new HttpClient(handlerMock));
 
-			//Assert
-			Assert.Equal(result.Name, existentName);
-			Assert.Equal(result.Description, descriptionTranlsated);
-			Assert.NotEqual(result.Description, description);
-		}
+            httpClientFactoryMock.Setup(x => x.CreateClient(funTranslationsClientName))
+                .Returns(new HttpClient(handlerMock));
 
-		private static PokemonApiResponse GetFakePokemonApiResponse(string name, string description)
-		{
-			return new PokemonApiResponse
-			{
-				Name = name,
-				FlavorTextEntries = [new FlavorTextEntry { FlavorText = description, Language = new Language { Name = Enums.Language.En.ToString() } }],
-				Habitat = new Habitat { Name = Enums.Habitat.Cave.ToString() },
-				IsLegendary = true,
-			};
-		}
+            //Act
+            var result = await pokemonService.TranslateAsync(existentName);
 
-		private static FunTranslationsResponse GetFakeFunTranslationsApiResponse(string description, string descriptionTranslated)
-		{
-			return new FunTranslationsResponse
-			{
-				Success = new Success { Total = 1 },
-				Contents = new Contents { Text = description, Translated = descriptionTranslated }
-			};
-		}
-	}
+            //Assert
+            Assert.Equal(result.Name, existentName);
+            Assert.Equal(result.Description, descriptionTranlsated);
+            Assert.NotEqual(result.Description, description);
+        }
+
+        private static PokemonApiResponse GetFakePokemonApiResponse(string name, string description)
+        {
+            return new PokemonApiResponse
+            {
+                Name = name,
+                FlavorTextEntries = [new FlavorTextEntry { FlavorText = description, Language = new Language { Name = Enums.Language.En.ToString() } }],
+                Habitat = new Habitat { Name = Enums.Habitat.Cave.ToString() },
+                IsLegendary = true,
+            };
+        }
+
+        private static FunTranslationsResponse GetFakeFunTranslationsApiResponse(string description, string descriptionTranslated)
+        {
+            return new FunTranslationsResponse
+            {
+                Success = new Success { Total = 1 },
+                Contents = new Contents { Text = description, Translated = descriptionTranslated }
+            };
+        }
+    }
 }
--- src/Pokedex/Services/PokemonService.cs
+++ src/Pokedex/Services/PokemonService.cs
@@ -5,112 +5,121 @@ using System.Text.RegularExpressions;
 
 namespace Pokedex.Services
 {
-	public class PokemonService(IHttpClientFactory httpClientFactory) : IPokemonService
-	{
-		private readonly IHttpClientFactory httpClientFactory = httpClientFactory;
+    public class PokemonService(ILogger<PokemonService> logger, IHttpClientFactory httpClientFactory) : IPokemonService
+    {
+        private readonly ILogger<PokemonService> logger = logger;
+        private readonly IHttpClientFactory httpClientFactory = httpClientFactory;
 
-		private readonly JsonSerializerOptions jsonSerializerOptions = new()
-		{
-			PropertyNameCaseInsensitive = true
-		};
+        private readonly JsonSerializerOptions jsonSerializerOptions = new()
+        {
+            PropertyNameCaseInsensitive = true
+        };
 
-		public async Task<PokemonDto> GetPokemonAsync(string name)
-		{
-			if (string.IsNullOrWhiteSpace(name))
-			{
-				throw new ArgumentNullException(nameof(name), $"{nameof(name)} cannot be empty.");
-			}
+        public async Task<PokemonDto> GetPokemonAsync(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException(nameof(name), $"{nameof(name)} cannot be empty.");
+            }
 
-			PokemonApiResponse pokemonApiResponse = await GetPokemonFromApi(name);
-			PokemonDto pokemonDto = ApiResponseToDto(pokemonApiResponse);
-			return pokemonDto;
-		}
+            PokemonApiResponse pokemonApiResponse = await GetPokemonFromApi(name);
+            PokemonDto pokemonDto = ApiResponseToDto(pokemonApiResponse);
+            return pokemonDto;
+        }
 
-		public async Task<PokemonDto> TranslateAsync(string name)
-		{
-			if (string.IsNullOrWhiteSpace(name))
-			{
-				throw new ArgumentNullException(nameof(name), $"{nameof(name)} cannot be empty.");
-			}
+        public async Task<PokemonDto> TranslateAsync(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException(nameof(name), $"{nameof(name)} cannot be empty.");
+            }
 
-			PokemonApiResponse pokemonApiResponse = await GetPokemonFromApi(name);
+            PokemonApiResponse pokemonApiResponse = await GetPokemonFromApi(name);
 
-			PokemonDto pokemonDto = ApiResponseToDto(pokemonApiResponse);
+            PokemonDto pokemonDto = ApiResponseToDto(pokemonApiResponse);
 
-			string translationType = TranslationType.Shakespeare.ToString().ToLower();
+            string translationType = TranslationType.Shakespeare.ToString().ToLower();
 
-			if (string.Equals(pokemonDto.Habitat, Enums.Habitat.Cave.ToString(), StringComparison.CurrentCultureIgnoreCase) || pokemonDto.IsLegendary)
-			{
-				translationType = TranslationType.Yoda.ToString().ToLower();
-			}
+            if (string.Equals(pokemonDto.Habitat, Enums.Habitat.Cave.ToString(), StringComparison.CurrentCultureIgnoreCase) || pokemonDto.IsLegendary)
+            {
+                translationType = TranslationType.Yoda.ToString().ToLower();
+            }
 
-			string cleanedDescription = CleanText(pokemonDto.Description ?? string.Empty);
-			FunTranslationsResponse funTranslationsResponse = await GetTranslationFromApi(translationType, cleanedDescription);
-			if (funTranslationsResponse.Success.Total == 1)
-			{
-				pokemonDto.Description = funTranslationsResponse.Contents.Translated;
-			}
+            string cleanedDescription = CleanText(pokemonDto.Description ?? string.Empty);
+            FunTranslationsResponse funTranslationsResponse = await GetTranslationFromApi(translationType, cleanedDescription);
+            if (funTranslationsResponse is not null && funTranslationsResponse.Success.Total == 1)
+            {
+                pokemonDto.Description = funTranslationsResponse.Contents.Translated;
+            }
 
-			return pokemonDto;
-		}
+            return pokemonDto;
+        }
 
-		private async Task<PokemonApiResponse> GetPokemonFromApi(string name)
-		{
-			var apiUri = $"https://pokeapi.co/api/v2/pokemon-species/{name}";
+        private async Task<PokemonApiResponse> GetPokemonFromApi(string name)
+        {
+            var apiUri = $"https://pokeapi.co/api/v2/pokemon-species/{name}";
 
-			var httpClient = httpClientFactory.CreateClient("PokeApi");
-			var httpResponse = await httpClient.GetAsync(apiUri);
+            var httpClient = httpClientFactory.CreateClient("PokeApi");
+            var httpResponse = await httpClient.GetAsync(apiUri);
 
-			if (!httpResponse.IsSuccessStatusCode)
-			{
-				if (httpResponse.StatusCode == System.Net.HttpStatusCode.NotFound)
-				{
-					throw new KeyNotFoundException($"The given key [{name}] was not found calling the pokeapi service.");
-				}
+            if (!httpResponse.IsSuccessStatusCode)
+            {
+                if (httpResponse.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    throw new KeyNotFoundException($"The given key [{name}] was not found calling the pokeapi service.");
+                }
 
-				var error = await httpResponse.Content.ReadAsStringAsync();
-				throw new Exception($"An error occured calling the pokeapi service: [{error}]");
-			}
+                var error = await httpResponse.Content.ReadAsStringAsync();
+                throw new Exception($"An error occured calling the pokeapi service: [{error}]");
+            }
 
-			var apiResponseRaw = await httpResponse.Content.ReadAsStringAsync();
+            var apiResponseRaw = await httpResponse.Content.ReadAsStringAsync();
 
-			var apiResponseDto = JsonSerializer.Deserialize<PokemonApiResponse>(apiResponseRaw, jsonSerializerOptions);
-			return apiResponseDto;
-		}
+            var apiResponseDto = JsonSerializer.Deserialize<PokemonApiResponse>(apiResponseRaw, jsonSerializerOptions);
+            return apiResponseDto;
+        }
 
-		private async Task<FunTranslationsResponse> GetTranslationFromApi(string translationType, string text)
-		{
-			var apiUri = $"https://api.funtranslations.com/translate/{translationType}.json?text={text}";
+        private async Task<FunTranslationsResponse> GetTranslationFromApi(string translationType, string text)
+        {
+            try
+            {
+                var apiUri = $"https://api.funtranslations.com/translate/{translationType}.json?text={text}";
 
-			var httpClient = httpClientFactory.CreateClient("FunTranslations");
-			var httpResponse = await httpClient.GetAsync(apiUri);
+                var httpClient = httpClientFactory.CreateClient("FunTranslations");
+                var httpResponse = await httpClient.GetAsync(apiUri);
 
-			if (!httpResponse.IsSuccessStatusCode)
-			{
-				var error = await httpResponse.Content.ReadAsStringAsync();
-				throw new Exception($"An error occured calling the pokeapi service: [{error}]");
-			}
+                if (!httpResponse.IsSuccessStatusCode)
+                {
+                    var error = await httpResponse.Content.ReadAsStringAsync();
+                    throw new Exception($"The response from funtranslations service is in error: [{error}]");
+                }
 
-			var apiResponseRaw = await httpResponse.Content.ReadAsStringAsync();
+                var apiResponseRaw = await httpResponse.Content.ReadAsStringAsync();
 
-			var apiResponseDto = JsonSerializer.Deserialize<FunTranslationsResponse>(apiResponseRaw, jsonSerializerOptions);
-			return apiResponseDto;
-		}
+                var apiResponseDto = JsonSerializer.Deserialize<FunTranslationsResponse>(apiResponseRaw, jsonSerializerOptions);
+                return apiResponseDto;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Exception message:{exceptionMessage}", ex.Message);
+                return null;
+            }
+        }
 
-		private static PokemonDto ApiResponseToDto(PokemonApiResponse pokemonApiResponse)
-		{
-			return new PokemonDto
-			{
-				Name = pokemonApiResponse.Name,
-				Description = pokemonApiResponse.FlavorTextEntries?.FirstOrDefault(e => string.Equals(e.Language?.Name, Enums.Language.En.ToString(), StringComparison.CurrentCultureIgnoreCase))?.FlavorText,
-				Habitat = pokemonApiResponse.Habitat?.Name,
-				IsLegendary = pokemonApiResponse.IsLegendary
-			};
-		}
+        public static PokemonDto ApiResponseToDto(PokemonApiResponse pokemonApiResponse)
+        {
+            return new PokemonDto
+            {
+                Name = pokemonApiResponse.Name,
+                Description = pokemonApiResponse.FlavorTextEntries?.FirstOrDefault(e => string.Equals(e.Language?.Name, Enums.Language.En.ToString(), StringComparison.CurrentCultureIgnoreCase))?.FlavorText,
+                Habitat = pokemonApiResponse.Habitat?.Name,
+                IsLegendary = pokemonApiResponse.IsLegendary
+            };
+        }
 
-		private static string CleanText(string text)
-		{
-			return Regex.Replace(text, @"\n|\f", " ");
-		}
-	}
+        private static string CleanText(string text)
+        {
+            return Regex.Replace(text, @"\n|\f", " ");
+        }
+    }
 }
